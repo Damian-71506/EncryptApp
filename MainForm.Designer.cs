c517854a994839144f7e624d72b553448065003b
Damian 71506 <gdx162129@student.gdansk.merito.pl>
2024-06-02 14:32:57 +0200

    Dodaj pliki projektów.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace App
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encryptFileButton = new System.Windows.Forms.Button();
+            this.decryptFileButton = new System.Windows.Forms.Button();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sendToServerButton = new System.Windows.Forms.Button();
+            this.encryptFolderButton = new System.Windows.Forms.Button();
+            this.decryptFolderButton = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // encryptFileButton
+            // 
+            this.encryptFileButton.Location = new System.Drawing.Point(20, 88);
+            this.encryptFileButton.Name = "encryptFileButton";
+            this.encryptFileButton.Size = new System.Drawing.Size(111, 33);
+            this.encryptFileButton.TabIndex = 0;
+            this.encryptFileButton.Text = "Encrypt File";
+            this.encryptFileButton.UseVisualStyleBackColor = true;
+            // 
+            // decryptFileButton
+            // 
+            this.decryptFileButton.Location = new System.Drawing.Point(137, 88);
+            this.decryptFileButton.Name = "decryptFileButton";
+            this.decryptFileButton.Size = new System.Drawing.Size(111, 33);
+            this.decryptFileButton.TabIndex = 1;
+            this.decryptFileButton.Text = "Decrypt File";
+            this.decryptFileButton.UseVisualStyleBackColor = true;
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "Aes",
+            "DES",
+            "RC2",
+            "Rijndael",
+            "TripleDES"});
+            this.comboBox.Location = new System.Drawing.Point(20, 43);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(228, 21);
+            this.comboBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Encryption Method";
+            // 
+            // sendToServerButton
+            // 
+            this.sendToServerButton.Location = new System.Drawing.Point(20, 166);
+            this.sendToServerButton.Name = "sendToServerButton";
+            this.sendToServerButton.Size = new System.Drawing.Size(228, 33);
+            this.sendToServerButton.TabIndex = 6;
+            this.sendToServerButton.Text = "Send to Server";
+            this.sendToServerButton.UseVisualStyleBackColor = true;
+            // 
+            // encryptFolderButton
+            // 
+            this.encryptFolderButton.Location = new System.Drawing.Point(20, 127);
+            this.encryptFolderButton.Name = "encryptFolderButton";
+            this.encryptFolderButton.Size = new System.Drawing.Size(111, 33);
+            this.encryptFolderButton.TabIndex = 7;
+            this.encryptFolderButton.Text = "Encrypt Folder";
+            this.encryptFolderButton.UseVisualStyleBackColor = true;
+            // 
+            // decryptFolderButton
+            // 
+            this.decryptFolderButton.Location = new System.Drawing.Point(137, 127);
+            this.decryptFolderButton.Name = "decryptFolderButton";
+            this.decryptFolderButton.Size = new System.Drawing.Size(111, 33);
+            this.decryptFolderButton.TabIndex = 8;
+            this.decryptFolderButton.Text = "Decrypt Folder";
+            this.decryptFolderButton.UseVisualStyleBackColor = true;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(17, 234);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(0, 13);
+            this.labelInfo.TabIndex = 10;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 274);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.decryptFolderButton);
+            this.Controls.Add(this.encryptFolderButton);
+            this.Controls.Add(this.sendToServerButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.decryptFileButton);
+            this.Controls.Add(this.encryptFileButton);
+            this.Name = "MainForm";
+            this.Text = "App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encryptFileButton;
+        private System.Windows.Forms.Button decryptFileButton;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button sendToServerButton;
+        private System.Windows.Forms.Button encryptFolderButton;
+        private System.Windows.Forms.Button decryptFolderButton;
+        private System.Windows.Forms.Label labelInfo;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,251 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System;
+
+namespace App
+{
+    public partial class MainForm : Form
+    {
+        private byte[] key;
+        private byte[] iv;
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            encryptFileButton.Click += EncryptFileButton_Click;
+            decryptFileButton.Click += DecryptFileButton_Click;
+            encryptFolderButton.Click += EncryptFolderButton_Click;
+            decryptFolderButton.Click += DecryptFolderButton_Click;
+            sendToServerButton.Click += SendToServerButton_Click;
+        }
+
+        private void SendToServerButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Multiselect = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string[] filePaths = openFileDialog.FileNames;
+                    Task.Run(() => {
+                        foreach (string filePath in filePaths)
+                        {
+                            SendFileToServer(filePath);
+                        }
+                        UpdateStatus("Pomyślnie wysłano pliki na serwer.");
+                    });
+                }
+            }
+        }
+
+        private async void SendFileToServer(string filePath)
+        {
+            try
+            {
+                using (var content = new MultipartFormDataContent())
+                {
+                    var fileContent = new ByteArrayContent(File.ReadAllBytes(filePath));
+                    fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+                    content.Add(fileContent, "file", Path.GetFileName(filePath));
+                
+                    var response = await httpClient.PostAsync("http://yourserver.com/upload", content); // Zastąp właściwym adresem URL swojego serwera. Upewnij się, że serwer jest skonfigurowany do odbierania plików przez POST.
+                    response.EnsureSuccessStatusCode();
+                }
+            }
+            catch (Exception ex)
+            {
+                UpdateStatus("Wystąpił błąd podczas wysyłania pliku: " + ex.Message);
+            }
+        }
+
+        private void EncryptFileButton_Click(object sender, EventArgs e)
+        {
+            string algorithm = comboBox.SelectedItem.ToString();
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    string destinationFilePath = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + "_encrypted" + Path.GetExtension(filePath));
+
+                    Task.Run(() => {
+                        EncryptFile(filePath, destinationFilePath, algorithm);
+                        UpdateStatus("Pomyślnie zaszyfrowano dane.");
+                    });
+                }
+            }
+        }
+
+        private void DecryptFileButton_Click(object sender, EventArgs e)
+        {
+            string algorithm = comboBox.SelectedItem.ToString();
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+                    string fileExtension = Path.GetExtension(filePath);
+
+                    if (fileNameWithoutExtension.EndsWith("_encrypted"))
+                    {
+                        fileNameWithoutExtension = fileNameWithoutExtension.Substring(0, fileNameWithoutExtension.Length - "_encrypted".Length);
+                    }
+
+                    string destinationFilePath = Path.Combine(Path.GetDirectoryName(filePath), fileNameWithoutExtension + "_decrypted" + fileExtension);
+
+                    Task.Run(() => {
+                        DecryptFile(filePath, destinationFilePath, algorithm);
+                        UpdateStatus("Pomyślnie odszyfrowano dane.");
+                    });
+                }
+            }
+        }
+
+        private void EncryptFolderButton_Click(object sender, EventArgs e)
+        {
+            string algorithm = comboBox.SelectedItem.ToString();
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string folderPath = folderDialog.SelectedPath;
+                    string destinationFolderPath = folderPath + "_encrypted";
+
+                    Task.Run(() => {
+                        EncryptFolder(folderPath, destinationFolderPath, algorithm);
+                        UpdateStatus("Pomyślnie zaszyfrowano folder.");
+                    });
+                }
+            }
+        }
+
+        private void DecryptFolderButton_Click(object sender, EventArgs e)
+        {
+            string algorithm = comboBox.SelectedItem.ToString();
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string folderPath = folderDialog.SelectedPath;
+                    string destinationFolderPath = folderPath;
+                    if (folderPath.EndsWith("_encrypted"))
+                    {
+                        destinationFolderPath = folderPath.Substring(0, folderPath.Length - "_encrypted".Length) + "_decrypted";
+                    }
+                    else
+                    {
+                        destinationFolderPath += "_decrypted";
+                    }
+
+                    Task.Run(() => {
+                        DecryptFolder(folderPath, destinationFolderPath, algorithm);
+                        UpdateStatus("Pomyślnie odszyfrowano folder.");
+                    });
+                }
+            }
+        }
+
+        private SymmetricAlgorithm GetAlgorithmByName(string algorithmName)
+        {
+            switch (algorithmName)
+            {
+                case "Aes":
+                    return Aes.Create();
+                case "DES":
+                    return DES.Create();
+                case "RC2":
+                    return RC2.Create();
+                case "Rijndael":
+                    return Rijndael.Create();
+                case "TripleDES":
+                    return TripleDES.Create();
+                default:
+                    throw new ArgumentException("Nieznany algorytm.");
+            }
+        }
+
+        private void EncryptFile(string inputFilePath, string outputFilePath, string algorithmName)
+        {
+            using (SymmetricAlgorithm algorithm = GetAlgorithmByName(algorithmName))
+            {
+                algorithm.GenerateKey();
+                algorithm.GenerateIV();
+
+                key = algorithm.Key;
+                iv = algorithm.IV;
+
+                using (FileStream inputFileStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read))
+                using (FileStream outputFileStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write))
+                using (CryptoStream cryptoStream = new CryptoStream(outputFileStream, algorithm.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    inputFileStream.CopyTo(cryptoStream);
+                }
+
+                File.WriteAllBytes(outputFilePath + ".key", key);
+                File.WriteAllBytes(outputFilePath + ".iv", iv);
+            }
+        }
+
+        private void DecryptFile(string inputFilePath, string outputFilePath, string algorithmName)
+        {
+            using (SymmetricAlgorithm algorithm = GetAlgorithmByName(algorithmName))
+            {
+                key = File.ReadAllBytes(inputFilePath + ".key");
+                iv = File.ReadAllBytes(inputFilePath + ".iv");
+
+                algorithm.Key = key;
+                algorithm.IV = iv;
+
+                using (FileStream inputFileStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read))
+                using (FileStream outputFileStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write))
+                using (CryptoStream cryptoStream = new CryptoStream(inputFileStream, algorithm.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    cryptoStream.CopyTo(outputFileStream);
+                }
+            }
+        }
+
+        private void EncryptFolder(string inputFolderPath, string outputFolderPath, string algorithmName)
+        {
+            Directory.CreateDirectory(outputFolderPath);
+
+            foreach (string filePath in Directory.GetFiles(inputFolderPath))
+            {
+                string fileName = Path.GetFileName(filePath);
+                string destFilePath = Path.Combine(outputFolderPath, fileName + ".encrypted");
+
+                EncryptFile(filePath, destFilePath, algorithmName);
+            }
+        }
+
+        private void DecryptFolder(string inputFolderPath, string outputFolderPath, string algorithmName)
+        {
+            Directory.CreateDirectory(outputFolderPath);
+
+            foreach (string filePath in Directory.GetFiles(inputFolderPath, "*.encrypted"))
+            {
+                string fileName = Path.GetFileNameWithoutExtension(filePath);
+                string destFilePath = Path.Combine(outputFolderPath, fileName);
+
+                DecryptFile(filePath, destFilePath, algorithmName);
+            }
+        }
+
+        private void UpdateStatus(string message)
+        {
+            if (labelInfo.InvokeRequired)
+            {
+                labelInfo.Invoke(new Action(() => labelInfo.Text = message));
+            }
+            else
+            {
+                labelInfo.Text = message;
+            }
+        }
+    }
+}
